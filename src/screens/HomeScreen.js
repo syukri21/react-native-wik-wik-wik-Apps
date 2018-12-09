3027139a2a054a0d29089dbf35ab5d574ece09af
syukri21 <syukrihsb148@gmail.com>
2018-12-09 12:40:35 +0700

    first config

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+class HomeScreen extends React.Component {
+	render() {
+		return <Text>Ini Home</Text>;
+	}
+}
+
+export default HomeScreen;
