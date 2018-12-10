ef76bd6a3194cb8af0eadb6706485cf8c129d434
syukri21 <syukrihsb148@gmail.com>
2018-12-10 16:55:51 +0700

    add backgrouns

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,37 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Image, Text, StyleSheet, Dimensions } from 'react-native';
+
+const width = Dimensions.get('screen').width;
+const height = Dimensions.get('screen').height;
+
 class HomeScreen extends React.Component {
 	render() {
-		return <Text>Ini Home</Text>;
+		return (
+			<View style={styles.container}>
+				<Image
+					style={styles.absolute(width, height)}
+					source={require('../asets/background.jpg')}
+					resizeMode='stretch'
+				/>
+			</View>
+		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container : {
+		position        : 'absolute',
+		left            : 0,
+		top             : 0,
+		right           : 0,
+		bottom          : 0,
+		backgroundColor : 'red'
+	},
+	absolute  : (width, height) => ({
+		position : 'absolute',
+		width,
+		height
+	})
+});
 
 export default HomeScreen;
