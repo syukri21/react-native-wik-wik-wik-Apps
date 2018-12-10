97b311bf900d68d3d787fa0071ccd369752d952d
syukri21 <syukrihsb148@gmail.com>
2018-12-10 21:07:36 +0700

    fix button for game

---
+++ src/components/ButtonMod.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, TouchableWithoutFeedback, Image, StyleSheet } from 'react-native';
+import * as Animatable from 'react-native-animatable';
+
+const color = {
+	blue   : require('../asets/buttonblue.png'),
+	green  : require('../asets/buttongreen.png'),
+	violet : require('../asets/buttonviolet.png'),
+	orange : require('../asets/buttonorange.png')
+};
+
+export default class ButtonMod extends React.Component {
+	handleViewRef = (ref) => (this.view = ref);
+
+	bounce = () => this.view.bounce(1000);
+
+	render() {
+		return (
+			<View style={styles.Touchable(this.props.position)}>
+				<TouchableWithoutFeedback
+					accessibilityIgnoresInvertColors={false}
+					style={styles.Touchable(this.props.position)}
+					onPress={this.bounce}
+				>
+					<Animatable.View style={styles.button} ref={this.handleViewRef}>
+						<Image
+							source={color[this.props.color]}
+							resizeMode='contain'
+							style={styles.image}
+						/>
+					</Animatable.View>
+				</TouchableWithoutFeedback>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	Touchable : ({ x, y }) => ({
+		width        : 80,
+		height       : 80,
+		borderRadius : 60,
+		position     : 'absolute',
+		bottom       : y,
+		left         : x
+	}),
+	button    : {
+		width        : 80,
+		height       : 80,
+		overflow     : 'hidden',
+		borderRadius : 60
+	},
+	image     : {
+		width     : 80,
+		height    : 80,
+
+		transform : [
+			{ translateY: 0 },
+			{ translateX: 0 }
+		]
+	}
+});
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,36 +1,64 @@
 import React from 'react';
 import { View, Image, Text, StyleSheet, Dimensions } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import { Container } from 'native-base';
+import ButtonMod from '../components/ButtonMod';
 
-const width = Dimensions.get('screen').width;
-const height = Dimensions.get('screen').height;
+const { width, height } = Dimensions.get('screen');
 
 class HomeScreen extends React.Component {
+	static navigationOptions = {
+		title  : 'HOME',
+		header : null
+	};
+
+	getPosition = (number) => {
+		let restWidth = width - 80 * 4;
+		x = restWidth / 5;
+		y = 20;
+		return {
+			x : (number + 1) * x + number * 80,
+			y : y
+		};
+	};
+
 	render() {
 		return (
-			<View style={styles.container}>
+			<Container style={styles.container}>
 				<Image
 					style={styles.absolute(width, height)}
 					source={require('../asets/background.jpg')}
 					resizeMode='stretch'
 				/>
-			</View>
+				<LinearGradient
+					colors={[
+						'#AE427388',
+						'#AE427388',
+						'#ffffffaa'
+					]}
+					style={styles.absolute(width, height)}
+				/>
+				<ButtonMod color='orange' position={this.getPosition(0)} />
+				<ButtonMod color='blue' position={this.getPosition(1)} />
+				<ButtonMod color='violet' position={this.getPosition(2)} />
+				<ButtonMod color='green' position={this.getPosition(3)} />
+			</Container>
 		);
 	}
 }
 
 const styles = StyleSheet.create({
 	container : {
-		position        : 'absolute',
-		left            : 0,
-		top             : 0,
-		right           : 0,
-		bottom          : 0,
-		backgroundColor : 'red'
+		position : 'absolute',
+		left     : 0,
+		top      : 0,
+		right    : 0,
+		bottom   : 0
 	},
 	absolute  : (width, height) => ({
 		position : 'absolute',
-		width,
-		height
+		width    : '100%',
+		height   : '100%'
 	})
 });
 
