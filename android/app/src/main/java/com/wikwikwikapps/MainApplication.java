0f50bb83531d3702bda061c7f560faba502d9751
syukri21 <syukrihsb148@gmail.com>
2018-12-14 08:39:57 +0700

    successful add fbsdk

--- android/app/src/main/java/com/wikwikwikapps/MainApplication.java
+++ android/app/src/main/java/com/wikwikwikapps/MainApplication.java
@@ -3,6 +3,7 @@ package com.wikwikwikapps;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.horcrux.svg.SvgPackage;
 import com.moussawi7.controlledgif.ControlledGifPackage;
@@ -12,13 +13,30 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+  
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
+
+  
+ 
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+  
+
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -28,6 +46,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(mCallbackManager),
             new RNSoundPackage(),
             new SvgPackage(),
             new ControlledGifPackage(),
@@ -51,5 +70,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    AppEventsLogger.activateApp(this);
   }
 }
